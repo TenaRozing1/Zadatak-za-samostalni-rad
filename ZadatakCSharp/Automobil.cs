8bb11ce60d32e83b261a0c2237e3728cec73619e
Tena Rozing <tena.rozing@gauss.dev>
2023-01-29 16:31:06 +0100

    Added Task 2.

---
+++ ZadatakCSharp/Automobil.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadatakCSharp
+{
+    internal class Automobil
+    {
+  
+        public delegate void NaPromjenuGodineProizvodnjeDelegat(object sender, EventArgs e);
+        public event NaPromjenuGodineProizvodnjeDelegat NaPromjenuGodineProizvodnje;
+        private string naziv;
+        private int godinaProizvodnje;
+        private double osnovnaCijena;
+
+    
+        public string Naziv { get => naziv; set => naziv = value; }
+        public int GodinaProizvodnje
+        {
+            get { return godinaProizvodnje; }
+            set
+            {
+                godinaProizvodnje = value;
+                if (NaPromjenuGodineProizvodnje != null)
+                {
+                    NaPromjenuGodineProizvodnje(this, new EventArgs());
+                }
+            }
+
+        }
+        public double OsnovnaCijena { get => osnovnaCijena; set => osnovnaCijena = value; }
+
+        #region Metode
+        public int Starost()
+        {
+            DateTime trenutniDatum = DateTime.Now;
+            int trenutnaGodina = int.Parse(trenutniDatum.Year.ToString());
+
+            return (trenutnaGodina - GodinaProizvodnje);
+        }
+
+        public string UkupnaCijena()
+        {
+            if (OsnovnaCijena <= 70000)
+            {
+                return "Ukupna cijena: " + OsnovnaCijena * 1.3;
+            }
+            else if (OsnovnaCijena > 70000 && OsnovnaCijena < 100000)
+            {
+                return "Ukupna cijena: " + OsnovnaCijena * 1.4;
+            }
+            else
+            {
+                return "Ukupna cijena: " + OsnovnaCijena * 1.5;
+            }
+        }
+
+        #endregion
+    }
+}
--- ZadatakCSharp/Program.cs
+++ ZadatakCSharp/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using ZadatakCSharp;
+
+Automobil automobil = new Automobil();
+
+try
+{
+Console.Write("Unesi naziv automobila: ");
+automobil.Naziv = Console.ReadLine();
+
+Console.Write("Unesi godinu proizvodnje: ");
+automobil.NaPromjenuGodineProizvodnje += new Automobil.NaPromjenuGodineProizvodnjeDelegat(automobil_NaPromjenuGodineProizvodnje);
+automobil.GodinaProizvodnje = int.Parse(Console.ReadLine());
+
+
+Console.Write("Unesi osnovnu cijenu: ");
+automobil.OsnovnaCijena = double.Parse(Console.ReadLine());
+}
+catch (Exception e)
+{
+
+Console.WriteLine("Greška: " + e.Message);
+}
+
+FileStream fs = new FileStream("automobil.txt", FileMode.OpenOrCreate);
+StreamWriter sw = new StreamWriter(fs);
+sw.WriteLine(automobil.Naziv);
+sw.WriteLine(automobil.GodinaProizvodnje);
+sw.WriteLine(automobil.OsnovnaCijena);
+sw.WriteLine(automobil.UkupnaCijena());
+
+sw.Close();
+fs.Close();
+FileStream fs2 = new FileStream("automobil.txt", FileMode.Open);
+StreamReader sr = new StreamReader(fs2);
+
+while (sr.Peek() > -1)
+{
+Console.WriteLine(sr.ReadLine());
+}
+sr.Close();
+
+public static partial class Program
+{
+    static void automobil_NaPromjenuGodineProizvodnje(object sender, EventArgs e)
+    {
+        Console.WriteLine("Starost automobila: {0}", ((Automobil)sender).Starost());
+    }
+}
