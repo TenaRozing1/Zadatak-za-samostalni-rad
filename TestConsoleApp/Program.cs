e548516bbb48afccc7f3bf7e04f2e5f10c60f3d9
Tena Rozing <tena.rozing@gauss.dev>
2023-02-02 20:26:52 +0100

    Added Selenium test written in Console app

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+namespace SeleniumTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Test case started...");
+            
+            IWebDriver driver = new EdgeDriver();
+            
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            Thread.Sleep(2000);
+           
+            IWebElement ele = driver.FindElement(By.Name("q"));
+             
+            ele.SendKeys("javatpoint tutorials");
+            Thread.Sleep(2000);
+         
+            IWebElement ele1 = driver.FindElement(By.Name("btnK"));
+        
+            ele1.Click();
+            Thread.Sleep(3000);
+          
+            driver.Close();
+            Console.Write("Success! Test case ended");
+        }
+    }
+}
